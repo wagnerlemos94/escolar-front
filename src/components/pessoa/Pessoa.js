bd8c3119c7c22c213d39dac066ae2763e5d79b04
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-11-03 11:43:33 -0300

    Imlementa novos campos do formulario

--- src/components/pessoa/Pessoa.js
+++ src/components/pessoa/Pessoa.js
@@ -7,6 +7,7 @@ import Responsavel from './Responsavel';
 
 class Pessoa extends React.Component{
     state = {
+        tipoPessoa:null
     }
 
     constructor(){
@@ -17,44 +18,96 @@ class Pessoa extends React.Component{
     render(){
         return(            
             <Card className="mt-5" title="Pessoa">
-                <FormGroup label="Dados Pessois">
-                <div className="row mt-3">
-                    <div className="col-sm-12">
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">Nome</span>
-                            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                <form>
+                    <FormGroup label="Dados Pessois">
+                        <div className="row mt-3">
+                            <div className="col-12">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Nome</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-3">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">CPF</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-3">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">RG</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-3">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Nascimento</span>
+                                    <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <select className="form-select form-select" aria-label=".form-select-sm example" onChange={e => this.setState({tipoPessoa:e.target.value})}>
+                                    <option defaultValue selected >Selecione tipo de Pessoa</option>
+                                    <option value="1">Responsável</option>
+                                    <option value="2">Condutor</option>
+                                    <option value="3">Aluno</option>
+                                </select>
+                            </div>
+                            {this.state.tipoPessoa == 1?
+                                <Responsavel />
+                                :this.state.tipoPessoa == 2?
+                                <Responsavel />
+                                :false
+                            }
                         </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">CPF</span>
-                            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    </FormGroup>
+                    <FormGroup label="Endereço">
+                        <div className="row mt-3">
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">CEP</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">UF</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Cidade</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Bairro</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Rua</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-4">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Numero</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="input-group input-group mb-3">
+                                    <span className="input-group-text" id="inputGroup-sizing-sm">Complemento</span>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">RG</span>
-                            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
-                        </div>
-                    </div>
-                    <div className="col-sm">
-                        <div className="input-group input-group-sm mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">Nascimento</span>
-                            <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
-                        </div>
-                    </div>
-                    <div className="col-sm">
-                        <select className="form-select form-select-sm" aria-label=".form-select-sm example">
-                            <option defaultValue selected>Selecione tipo de Pessoa</option>
-                            <option value="1">Responsável</option>
-                            <option value="2">Condutor</option>
-                            <option value="3">Aluno</option>
-                        </select>
-                    </div>
-                </div>
-                <Responsavel></Responsavel>
-
-                </FormGroup>
+                    </FormGroup>
+                </form>
             </Card>
         );
     }
--- src/components/pessoa/Responsavel.js
+++ src/components/pessoa/Responsavel.js
@@ -13,11 +13,24 @@ class Responsavel extends React.Component{
         ]
     }
 
+    
     render(){
+        const element = <div>Hello World</div>;
         return(
-            <div className="col">
-                <Select options={this.state.parentesco} />
-            </div>
+            <>
+                <div className="col">
+                    <Select options={this.state.parentesco} />
+                </div>
+                <div className="col-12">
+                    <div className="col">
+                        <div className="input-group input-group mb-3">
+                            <span className="input-group-text" id="inputGroup-sizing-sm">Nome do Aluno</span>
+                            <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                            <i className="bi bi-plus-circle mx-2" onClick={e => alert()} style={{color: "blue",fontSize: "1.2rem"}}></i>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 
