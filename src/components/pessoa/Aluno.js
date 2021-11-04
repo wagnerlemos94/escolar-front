440e79efbf651379858c85dea54c7ff8cf5525fa
Wagner Cupertino Lemos <wagner.lemos@ses.ba>
2021-11-04 16:32:19 -0300

    Implementa novos campos do formulario

--- src/components/pessoa/Aluno.js
+++ src/components/pessoa/Aluno.js
@@ -15,43 +15,50 @@ class Aluno extends React.Component{
             <div className="col-12">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Colegio</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                     onChange={e => this.props.aluno.colegio = e.target.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Serie</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                     onChange={e => this.props.aluno.serie = e.target.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Turma</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                     onChange={e => this.props.aluno.turma = e.target.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Turno</span>
-                    <Select className="col" options={this.state.turno} />
+                    <Select className="col" options={this.state.turno} 
+                    onChange={e => this.props.aluno.turno = e.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Mãe</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                    onChange={e => this.props.aluno.mae = e.target.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Pai</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                    onChange={e => this.props.aluno.pai = e.target.value}/>
                 </div>
             </div>
             <div className="col-4">
                 <div className="input-group input-group mb-3">
                     <span className="input-group-text" id="inputGroup-sizing-sm">Convênio Médico</span>
-                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                    onChange={e => this.props.aluno.convenioMedico = e.target.value}/>
                 </div>
             </div>
             </>
--- src/components/pessoa/Pessoa.js
+++ src/components/pessoa/Pessoa.js
@@ -7,16 +7,41 @@ import Aluno from './Aluno';
 import Condutor from './Condutor';
 
 class Pessoa extends React.Component{
-    state = {
-        tipoPessoa:null,
-        title:"Pessoa"
-    }
-
     constructor(){
         super();
         // this.service = new UsuarioSerice;
     }
    
+    state = {
+        perfil:null,
+        title:"Pessoa",
+        pessoa:{
+            nome:"",
+            cpf:"",
+            rg:"",
+            nascimento:"",
+            enderecos:[]
+        },
+        endereco:{
+            cep:"",
+            uf:"",
+            cidade:"",
+            bairro:"" ,
+            rua:"" ,
+            numero:"", 
+            complemento:"" 
+        },aluno:{
+
+        },
+        contatos:[]
+    }
+
+    salvar = () => {
+        this.state.pessoa.enderecos.push(this.state.endereco);
+        const pessoa = this.state.pessoa;
+        console.log(pessoa);
+    }
+  
     render(){
         return(            
             <Card className="mt-5" title={this.state.title}>
@@ -26,31 +51,51 @@ class Pessoa extends React.Component{
                             <div className="col-12">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Nome</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                        onChange={e => this.setState({pessoa:{
+                                            ...this.state.pessoa,
+                                            nome: e.target.value
+                                        }})}
+                                    />
                                 </div>
                             </div>
                             <div className="col">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">CPF</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" 
+                                   onChange={e => this.setState({pessoa:{
+                                    ...this.state.pessoa,
+                                    cpf: e.target.value
+                                }})}
+                            />
                                 </div>
                             </div>
                             <div className="col">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">RG</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" 
+                                   onChange={e => this.setState({pessoa:{
+                                    ...this.state.pessoa,
+                                    rg: e.target.value
+                                }})}
+                            />
                                 </div>
                             </div>
                             <div className="col">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Nascimento</span>
-                                    <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="date" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" 
+                                   onChange={e => this.setState({pessoa:{
+                                    ...this.state.pessoa,
+                                    nascimento: e.target.value
+                                }})}
+                            />
                                 </div>
                             </div>
                             <div className="col">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Perfil</span>
-                                    <select className="form-select form-select" aria-label=".form-select-sm example" onChange={e => this.setState({tipoPessoa:e.target.value})}>
+                                    <select className="form-select form-select" aria-label=".form-select-sm example" onChange={e => this.setState({perfil:e.target.value})}>
                                         <option disabled selected >Selecione tipo de Pessoa</option>
                                         <option value="1">Responsável</option>
                                         <option value="2">Condutor</option>
@@ -58,12 +103,12 @@ class Pessoa extends React.Component{
                                     </select>
                                 </div>
                             </div>
-                            {this.state.tipoPessoa == 1?
+                            {this.state.perfil == 1?
                                 <Responsavel />
-                                :this.state.tipoPessoa == 2?
+                                :this.state.perfil == 2?
                                 <Condutor />
-                                :this.state.tipoPessoa == 3?
-                                <Aluno />
+                                :this.state.perfil == 3?
+                                <Aluno aluno={this.state.aluno}/>
                                 :false
                             }
                         </div>
@@ -73,47 +118,75 @@ class Pessoa extends React.Component{
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">CEP</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        cep:e.target.value
+                                        }})}/>
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">UF</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        uf:e.target.value}})}/>
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Cidade</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        cidade:e.target.value}})}/>
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Bairro</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        bairro:e.target.value}})}/>
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Rua</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        rua:e.target.value
+                                        }})}/>
                                 </div>
                             </div>
                             <div className="col-4">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Numero</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        numero:e.target.value
+                                        }})}/>
                                 </div>
                             </div>
                             <div className="col-12">
                                 <div className="input-group input-group mb-3">
                                     <span className="input-group-text" id="inputGroup-sizing-sm">Complemento</span>
-                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"/>
+                                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm"
+                                    onChange={e => this.setState({endereco:{
+                                        ...this.state.endereco,
+                                        complemento:e.target.value
+                                        }})}/>
                                 </div>
                             </div>
                         </div>
                     </FormGroup>
+                    <div>
+                        <button type="button" className="btn btn-primary" onClick={this.salvar}>Salvar</button>
+                    </div>
                 </form>
             </Card>
         );
